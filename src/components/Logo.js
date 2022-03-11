e624af9e40d63af433cff370c70b90dbaf8080e4
asketsystem <asketsystem1@gmail.com>
2022-03-11 16:37:10 +0000

    Logo Comp

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,6 +5,12 @@ import styled from "styled-components";
 const LogoText = styled.h1`
   font-family: "Akaya Telivigala", cursive;
   font-size: ${(props) => props.theme.fontxxxl};
+  color: ${(props) => props.theme.text};
+  transition: all 0.2s ease;
+
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
 
 const Logo = () => {
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -17,12 +17,43 @@ const NavBar = styled.nav`
   margin: 0 auto;
 `;
 
+const Menu = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+`;
+
+const MenuItem = styled.li`
+  margin: 0 1rem;
+  color: ${(props) => props.theme.text};
+  cursor: pointer;
+
+  &::after {
+    content: " ";
+    display: 100%;
+    height: 2px;
+    background: ${(props) => props.theme.text};
+    transition: width 0.3s ease;
+  }
+  &:hover::after {
+    width: 100%;
+  }
+`;
+
 const Navigation = () => {
   return (
     <Section>
       <NavBar>
         <Logo />
-        <h2>Menu</h2>
+        <Menu>
+          <MenuItem>Home</MenuItem>
+          <MenuItem>About</MenuItem>
+          <MenuItem>Roadmap</MenuItem>
+          <MenuItem>Showcase</MenuItem>
+          <MenuItem>Team</MenuItem>
+          <MenuItem>Faq</MenuItem>
+        </Menu>
         <h2>Button</h2>
       </NavBar>
     </Section>
