5dd1a0e321ae242a0037120071bec392707b50c6
asketsystem <asketsystem1@gmail.com>
2022-03-11 14:49:02 +0000

    Navigation comp

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  width: 100vw;
+  background-color: ${(props) => props.theme.body};
+`;
+
+const NavBar = styled.nav`
+  display: flex;
+  justify-centent: space-between;
+  align-items: center;
+
+  width: 85%;
+  height: 5rem;
+  margin: 0 auto;
+`;
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <Section>
+      <NavBar>
+        <h2>Logo</h2>
+        <h2>Menu</h2>
+        <h2>Button</h2>
+      </NavBar>
+    </Section>
+  );
 };
 
 export default Navigation;
